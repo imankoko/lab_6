aace1427c23606659906cbb3c5474ffa32d28fd5
Iman Fahmi <imanfahmi7171@gmail.com>
2025-06-02 22:35:42 +0800

    Associative array

---
+++ lab6_q2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 6 Q2</title>
+</head>
+<body>
+    <?php
+    $students = [
+        [
+            'name' => 'Alice',
+            'program' => 'BIP',
+            'age' => 21
+        ],
+        [
+            'name' => 'Bob',
+            'program' => 'BIS',
+            'age' => 20
+        ],
+        [
+            'name' => 'Raju',
+            'program' => 'BIT',
+            'age' => 22
+        ]
+    ];
+    ?>
+</body>
+</html>
