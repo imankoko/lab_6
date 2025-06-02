f28d12a00b87381d0fdb29e479dc56554ddf5b97
Iman Fahmi <imanfahmi7171@gmail.com>
2025-06-02 22:52:31 +0800

    Calculate function

---
+++ lab6_q3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 6 Q3</title>
+</head>
+<body>
+    <?php
+    function calculateArea($width, $height) {
+        return $width * $height;
+    }
+    ?>
+</body>
+</html>
